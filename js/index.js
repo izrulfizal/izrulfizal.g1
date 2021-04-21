d6c71204278e1788fada7de16e4f9e26a0bee9d3
izrulfizal <izrulfizal1410@gmail.com>
2021-04-21 16:06:35 +0800

    added start panel

--- js/index.js
+++ js/index.js
@@ -14,10 +14,21 @@ function startTime() {
     document.getElementById('timeBar').innerHTML =
     h + ":" + m + " " + ampm;
     var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
+}
+
+function checkTime(i) {
     if (i < 10) {i = "0" + i};  
     return i;
-  }
+}
 
 startTime();
+document.getElementById('startBtn').onclick = function() {
+    
+    var startPanel = document.getElementById("startPanel");
+    if (startPanel.style.display === "block"){
+        startPanel.style.display = "none";
+    }
+    else{
+        startPanel.style.display = "block";
+    }
+}
