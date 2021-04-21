d5e582b6433971d838722c9493854c4546152975
izrulfizal <izrulfizal1410@gmail.com>
2021-04-21 14:06:28 +0800

    add Time function

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    if(h > 12){
+        ampm = "PM";
+    }
+    if(h < 12){
+        ampm = "AM";
+    }
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timeBar').innerHTML =
+    h + ":" + m + " " + ampm;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
+
+startTime();
